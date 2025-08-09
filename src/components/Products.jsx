f010a5d7a6794e2781527147320df403b9a5f788
George Bassey <georgebassey1602@gmail.com>
2025-08-09 13:42:48 +0100

    Updated product data, package.json, Products component, and added ProductsCard.css

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addCart } from "../redux/action";
+import { motion } from "framer-motion";
+import "../components/ProductsCard.css";
 
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -109,50 +111,44 @@ const Products = () => {
 
         {filter.map((product) => {
           return (
-            <div
-              id={product.id}
-              key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
-            >
-              <div className="card text-center h-100" key={product.id}>
-                <img
-                  className="card-img-top p-3"
-                  src={product.image}
-                  alt="Card"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {product.title.substring(0, 12)}...
-                  </h5>
-                  <p className="card-text">
-                    {product.description.substring(0, 90)}...
-                  </p>
+            <motion.div whileHover={{ y: -5 }} className="product-card">
+              <div className="image-container">
+                {/* badge */}
+                <span className="badge bestseller"> Best Seller</span>
+                <img src={product.image} alt="card" className="product-image" />
+              </div>
+              <div className="product-details">
+                <h2 className="brand-name">{product.title}</h2>
+                <div className="name-size-row">
+                  {/* select for varian */}
+                  <select className="size-select">
+                    <option>size</option>
+                    <option>7</option>
+                    <option>8.5</option>
+                    <option>9</option>
+                  </select>
                 </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">$ {product.price}</li>
-                  {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-                </ul>
-                <div className="card-body">
-                  <Link
-                    to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
-                  >
-                    Buy Now
-                  </Link>
-                  <button
-                    className="btn btn-dark m-1"
-                    onClick={() => {
-                      toast.success("Added to cart");
-                      addProduct(product);
-                    }}
-                  >
-                    Add to Cart
-                  </button>
+                <div className="price-cart-row">
+                  {/* price */}
+                  <p className="prize">${product.price}</p>
+                  {/* add to cart/out of stock button */}
+                  {product.length > 0 ? (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        toast.success("Added to cart");
+                        addProduct(product);
+                      }}
+                      className="add-to-cart"
+                    ></button>
+                  ) : (
+                    <button type="button" disabled className="out-of-stock">
+                      Out of Stock
+                    </button>
+                  )}
                 </div>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </>
@@ -176,3 +172,42 @@ const Products = () => {
 };
 
 export default Products;
+
+// {/* <div
+//   id={product.id}
+//   key={product.id}
+//   className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+// >
+//   <div className="card text-center h-100" key={product.id}>
+//     <img
+//       className="card-img-top p-3"
+//       src={product.image}
+//       alt="Card"
+//       height={300}
+//     />
+//     <div className="card-body">
+//       <h5 className="card-title">{product.title.substring(0, 12)}...</h5>
+//       <p className="card-text">{product.description.substring(0, 90)}...</p>
+//     </div>
+//     <ul className="list-group list-group-flush">
+//       <li className="list-group-item lead">$ {product.price}</li>
+//       {/* <li className="list-group-item">Dapibus ac facilisis in</li>
+//         <li className="list-group-item">Vestibulum at eros</li> */}
+//     </ul>
+//     <div className="card-body">
+//       <Link to={"/product/" + product.id} className="btn btn-dark m-1">
+//         Buy Now
+//       </Link>
+//       <button
+//         className="btn btn-dark m-1"
+//         onClick={() => {
+//           toast.success("Added to cart");
+//           addProduct(product);
+//         }}
+//       >
+//         Add to Cart
+//       </button>
+//       //{" "}
+//     </div>
+//   </div>
+// </div>; */}
